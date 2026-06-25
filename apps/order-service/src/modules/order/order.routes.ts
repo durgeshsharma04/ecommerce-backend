3d22e6e918f36c966847345de49fc64e9bf7a08a
durgeshsharma04 <sharx.durgesh@gmail.com>
2026-06-26 05:04:49 +0530

    change in text

--- apps/order-service/src/modules/order/order.routes.ts
+++ apps/order-service/src/modules/order/order.routes.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.json({ message: "User service working" });
+  res.json({ message: "Order service working" });
 });
 
 export default router;
